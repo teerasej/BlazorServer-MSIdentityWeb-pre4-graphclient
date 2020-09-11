acccd5a1f98a02253bf21340f65c7d56791bd8f4
Teerasej <teerasej@gmail.com>
2020-09-11 23:38:58 +0700

    provide option to cache token in cosmosDB emulator

--- Startup.cs
+++ Startup.cs
@@ -33,18 +33,32 @@ namespace preview4blazorserver
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // services.AddCosmosCache((CosmosCacheOptions cacheOptions) =>
+            // {
+            //     cacheOptions.ContainerName = Configuration["CosmosCache:ContainerName"];
+            //     cacheOptions.DatabaseName = Configuration["CosmosCache:DatabaseName"];
+            //     cacheOptions.ClientBuilder = new CosmosClientBuilder(Configuration["CosmosCache:ConnectionString"]);
+            //     cacheOptions.CreateIfNotExists = true;
+            // });
+
             services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
                     .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"))
-                    .EnableTokenAcquisitionToCallDownstreamApi(
-                        new string[] { "User.Read", "File.ReadWrite", "File.Read" }
-                    )
+                    .EnableTokenAcquisitionToCallDownstreamApi()
                     .AddMicrosoftGraph()
                     .AddInMemoryTokenCaches()
+                    // .AddDistributedTokenCaches()
                     ;
 
 
             services.AddControllersWithViews()
                     .AddMicrosoftIdentityUI();
+            // services.AddControllersWithViews(options =>
+            // {
+            //     var policy = new AuthorizationPolicyBuilder()
+            //         .RequireAuthenticatedUser()
+            //         .Build();
+            //     options.Filters.Add(new AuthorizeFilter(policy));
+            // }).AddMicrosoftIdentityUI();
 
             services.AddAuthorization(options =>
             {
